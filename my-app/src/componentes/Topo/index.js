28d6778a5dd8c8a7cb7ca2ef6b66acf9c14825e9
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-04-12 14:03:30 -0300

    Primeiras Alterações no Topo

---
+++ my-app/src/componentes/Topo/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+const Topo = ()=> {
+    return(
+        <header className='topo-container'>
+        <div className='topo-conteudo'>
+            <h2>Filipe Sena</h2>
+
+            <nav className='topo-links'>
+            <a href='#' className='topo-link'>Home</a>
+            </nav>
+        </div>
+        </header>
+    );
+}
+
+export default Topo;
