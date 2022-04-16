49295c20de02e46c59831b3218cd5f648657f3b7
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-04-16 12:56:37 -0300

    Criando o card das Skills

--- my-app/src/pages/Skills/index.js
+++ my-app/src/pages/Skills/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-import './style.css';
+import './style.css'
+import ImagemHtml from '../img/html.png';
+import ImagemCSS from '../img/css.png';
+import ImagemJS from '../img/js.png';
+import ImagemReact from '../img/react.png';
+import ImgGit from '../img/git.png';
+import ImgNode from '../img/node.png';
 import EstruturaPagina from "../../componentes/EstruturaPagina";
 
 const Skills = ()=>{
@@ -9,6 +15,33 @@ const Skills = ()=>{
                 <div className="title">
                 <h1>Skills</h1>
                 </div>
+                <div className="card-container">
+                    <div className="card-skills">
+                        <img src={ImagemHtml}></img>
+                        <h2 className="title-card">HTML</h2>
+                    </div>
+                    <div className="card-skills">
+                    <img src={ImagemCSS}></img>
+                        <h2 className="title-card">CSS</h2>
+                    </div>
+                    <div className="card-skills">
+                    <img src={ImagemJS}></img>
+                        <h2 className="title-card">JavaScript</h2>
+                    </div>
+                    <div className="card-skills">
+                    <img src={ImagemReact}></img>
+                        <h2 className="title-card">ReactJS</h2>
+                    </div>
+                    <div className="card-skills">
+                    <img src={ImgGit}></img>
+                        <h2 className="title-card">GitHub</h2>
+                    </div>
+                    <div className="card-skills">
+                    <img src={ImgNode}></img>
+                        <h2 className="title-card">NodeJS</h2>
+                    </div>
+                </div>
+                
                 
             </div>
         </EstruturaPagina>
