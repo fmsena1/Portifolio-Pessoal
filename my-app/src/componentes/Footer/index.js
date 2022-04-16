7ad8938742efb2073ecc3b4032d49bae3f678a59
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-04-16 15:21:57 -0300

    Adicionando Contatos

--- my-app/src/componentes/Footer/index.js
+++ my-app/src/componentes/Footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './style.css';
-
+import ImgLinkedin from '../img/linkedin.png';
+import ImgGithub from '../../pages/img/git.png'
 
 const Footer = ()=>{
 
@@ -10,7 +11,8 @@ const Footer = ()=>{
                 <p>
                 Copyright 2022 / Portifolio 
                 </p>
-                
+                <a href="https://www.linkedin.com/in/filipe-magalh%C3%A3es-sena-859b53176/"><img  className='links'src={ImgLinkedin} alt="linkedin"></img></a>
+                <a href="https://github.com/fmsena1"><img  className='links'src={ImgGithub} alt="github"></img></a>
                 
             </div> 
         </footer>
